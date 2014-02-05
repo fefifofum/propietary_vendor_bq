1261547194f165fcb4b88738d2962ac8500efb98
fefifofum <fefifofum@email.com>
2014-02-05 04:11:46 +0100

    Initial cm-11.0 commit

--- maxwell2plus/maxwell2plus-vendor-blobs.mk
+++ maxwell2plus/maxwell2plus-vendor-blobs.mk
@@ -15,28 +15,27 @@
 
 # Binaries
 PRODUCT_COPY_FILES += \
-    vendor/bq/maxwell2plus/proprietary/bin/glgps:system/bin/glgps \
-    vendor/bq/maxwell2plus/proprietary/bin/vold:system/bin/vold
+    vendor/bq/maxwell2plus/proprietary/bin/glgps:system/bin/glgps
 
 # Libraries
 PRODUCT_COPY_FILES += \
     vendor/bq/maxwell2plus/proprietary/lib/libion.so:system/lib/libion.so \
     vendor/bq/maxwell2plus/proprietary/lib/libjpeghwenc.so:system/lib/libjpeghwenc.so \
     vendor/bq/maxwell2plus/proprietary/lib/libOMX_Core.so:system/lib/libOMX_Core.so \
-    vendor/bq/maxwell2plus/proprietary/lib/libomxvpu.so:system/lib/libomxvpu.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libomxvpu_dec.so:system/lib/libomxvpu_dec.so \
     vendor/bq/maxwell2plus/proprietary/lib/libomxvpu_enc.so:system/lib/libomxvpu_enc.so \
     vendor/bq/maxwell2plus/proprietary/lib/librkswscale.so:system/lib/librkswscale.so \
     vendor/bq/maxwell2plus/proprietary/lib/libvideoeditor_core.so:system/lib/libvideoeditor_core.so \
     vendor/bq/maxwell2plus/proprietary/lib/libvideoeditor_jni.so:system/lib/libvideoeditor_jni.so \
     vendor/bq/maxwell2plus/proprietary/lib/libvideoeditorplayer.so:system/lib/libvideoeditorplayer.so \
-    vendor/bq/maxwell2plus/proprietary/lib/libvpu.so:system/lib/libvpu.so \
-    vendor/bq/maxwell2plus/proprietary/lib/libyuvtorgb.so:system/lib/libyuvtorgb.so
+    vendor/bq/maxwell2plus/proprietary/lib/libvpu.so:system/lib/libvpu.so
 
 # Statefright
 PRODUCT_COPY_FILES += \
     vendor/bq/maxwell2plus/proprietary/lib/libapedec.so:system/lib/libapedec.so \
-    vendor/bq/maxwell2plus/proprietary/lib/libhtml5_check.so:system/lib/libhtml5_check.so \
     vendor/bq/maxwell2plus/proprietary/lib/libmediaplayerservice.so:system/lib/libmediaplayerservice.so \
+    vendor/bq/maxwell2plus/proprietary/lib/librk_demux.so:system/lib/librk_demux.so \
+    vendor/bq/maxwell2plus/proprietary/lib/librk_on2.so:system/lib/librk_on2.so \
     vendor/bq/maxwell2plus/proprietary/lib/librkwmapro.so:system/lib/librkwmapro.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright.so:system/lib/libstagefright.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefrighthw.so:system/lib/libstagefrighthw.so \
@@ -45,6 +44,7 @@ PRODUCT_COPY_FILES += \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_chromium_http.so:system/lib/libstagefright_chromium_http.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_enc_common.so:system/lib/libstagefright_enc_common.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_foundation.so:system/lib/libstagefright_foundation.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libstagefright_httplive.so:system/lib/libstagefright_httplive.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_omx.so:system/lib/libstagefright_omx.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_aacdec.so:system/lib/libstagefright_soft_aacdec.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_aacenc.so:system/lib/libstagefright_soft_aacenc.so \
@@ -53,6 +53,7 @@ PRODUCT_COPY_FILES += \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_amrwbenc.so:system/lib/libstagefright_soft_amrwbenc.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_flacenc.so:system/lib/libstagefright_soft_flacenc.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_g711dec.so:system/lib/libstagefright_soft_g711dec.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_gsmdec.so:system/lib/libstagefright_soft_gsmdec.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_h264dec.so:system/lib/libstagefright_soft_h264dec.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_h264enc.so:system/lib/libstagefright_soft_h264enc.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_mp3dec.so:system/lib/libstagefright_soft_mp3dec.so \
@@ -61,16 +62,19 @@ PRODUCT_COPY_FILES += \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_rawdec.so:system/lib/libstagefright_soft_rawdec.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_vorbisdec.so:system/lib/libstagefright_soft_vorbisdec.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_vpxdec.so:system/lib/libstagefright_soft_vpxdec.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libstagefright_soft_vpxenc.so:system/lib/libstagefright_soft_vpxenc.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_wfd.so:system/lib/libstagefright_wfd.so \
     vendor/bq/maxwell2plus/proprietary/lib/libstagefright_yuv.so:system/lib/libstagefright_yuv.so
 
-# DRM
+# Sensors
 PRODUCT_COPY_FILES += \
-    vendor/bq/maxwell2plus/proprietary/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-    vendor/bq/maxwell2plus/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-    vendor/bq/maxwell2plus/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-    vendor/bq/maxwell2plus/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
-    vendor/bq/maxwell2plus/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so
+    vendor/bq/maxwell2plus/proprietary/lib/libakm8963.so:system/lib/libakm8963.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libakmd.so:system/lib/libakmd.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libami.so:system/lib/libami.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libami_sensor_mw.so:system/lib/libami_sensor_mw.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libmllite.so:system/lib/libmllite.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so \
+    vendor/bq/maxwell2plus/proprietary/lib/libmplmpu.so:system/lib/libmplmpu.so
 
 # Mali
 PRODUCT_COPY_FILES += \
@@ -82,20 +86,23 @@ PRODUCT_COPY_FILES += \
 
 # HAL
 PRODUCT_COPY_FILES += \
+    vendor/bq/maxwell2plus/proprietary/lib/hw/audio.primary.rk30board.so:system/lib/hw/audio.primary.rk30board.so \
+    vendor/bq/maxwell2plus/proprietary/lib/hw/audio_policy.rk30board.so:system/lib/hw/audio_policy.rk30board.so \
     vendor/bq/maxwell2plus/proprietary/lib/hw/camera.rk30board.so:system/lib/hw/camera.rk30board.so \
     vendor/bq/maxwell2plus/proprietary/lib/hw/gps.rk30board.so:system/lib/hw/gps.rk30board.so \
     vendor/bq/maxwell2plus/proprietary/lib/hw/gpu.rk30board.so:system/lib/hw/gpu.rk30board.so \
     vendor/bq/maxwell2plus/proprietary/lib/hw/gralloc.rk30board.so:system/lib/hw/gralloc.rk30board.so \
-    vendor/bq/maxwell2plus/proprietary/lib/hw/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so
+    vendor/bq/maxwell2plus/proprietary/lib/hw/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so \
+    vendor/bq/maxwell2plus/proprietary/lib/hw/sensors.rk30board.so:system/lib/hw/sensors.rk30board.so
 
 # Modules
 PRODUCT_COPY_FILES += \
-    vendor/bq/maxwell2plus/proprietary/lib/modules/mali.ko:system/lib/modules/mali.ko \
-    vendor/bq/maxwell2plus/proprietary/lib/modules/rk29-ipp.ko:system/lib/modules/rk29-ipp.ko \
-    vendor/bq/maxwell2plus/proprietary/lib/modules/rk30_mirroring.ko.3.0.8+:system/lib/modules/rk30_mirroring.ko.3.0.8+ \
+    vendor/bq/maxwell2plus/proprietary/lib/modules/mali.ko.3.0.36+:system/lib/modules/mali.ko.3.0.36+ \
+    vendor/bq/maxwell2plus/proprietary/lib/modules/rk29-ipp.ko.3.0.36+:system/lib/modules/rk29-ipp.ko.3.0.36+ \
+    vendor/bq/maxwell2plus/proprietary/lib/modules/rk30_mirroring.ko.3.0.36+:system/lib/modules/rk30_mirroring.ko.3.0.36+ \
     vendor/bq/maxwell2plus/proprietary/lib/modules/rkwifi.ko:system/lib/modules/rkwifi.ko \
-    vendor/bq/maxwell2plus/proprietary/lib/modules/ump.ko:system/lib/modules/ump.ko \
-    vendor/bq/maxwell2plus/proprietary/lib/modules/vpu_service.ko:system/lib/modules/vpu_service.ko
+    vendor/bq/maxwell2plus/proprietary/lib/modules/ump.ko.3.0.36+:system/lib/modules/ump.ko.3.0.36+ \
+    vendor/bq/maxwell2plus/proprietary/lib/modules/vpu_service.ko.3.0.36+:system/lib/modules/vpu_service.ko.3.0.36+
 
 # Firmware
 PRODUCT_COPY_FILES += \
